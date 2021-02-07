2a59a7255efecd782f674a040703b55baa656864
Ashwin K Ashok <Ashwin Ashok>
2021-02-07 00:33:15 -0500

    Added new activity for clickyclick event. Laid out the view components.

---
+++ app/src/main/java/edu/neu/madcourse/numad21s_ashwinashok/ClickyClick.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad21s_ashwinashok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClickyClick extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clicky_click);
+    }
+}
