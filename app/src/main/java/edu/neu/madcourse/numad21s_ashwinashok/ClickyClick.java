45c6152401aa18aaa0d2f7dacd05c90eaa4aa5bb
Ashwin K Ashok <Ashwin Ashok>
2021-02-08 01:07:34 -0500

    added stated persistance for the last clicked button so that the state of the last clicked test view persists even after screen rotation.

--- app/src/main/java/edu/neu/madcourse/numad21s_ashwinashok/ClickyClick.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_ashwinashok/ClickyClick.java
@@ -1,5 +1,6 @@
 package edu.neu.madcourse.numad21s_ashwinashok;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -18,6 +19,8 @@ public class ClickyClick extends AppCompatActivity {
     private Button button_f;
     private Button clicky_click_back_button;
 
+    private String last_button_pressed;
+
     private TextView button_pressed_text;
 
     @Override
@@ -35,9 +38,15 @@ public class ClickyClick extends AppCompatActivity {
 
         button_pressed_text = findViewById(R.id.button_pressed_text_view);
 
+        if(savedInstanceState != null) {
+            last_button_pressed = savedInstanceState.getString("last_button_pressed");
+            button_pressed_text.setText(last_button_pressed);
+        }
+
         button_a.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                last_button_pressed = "Pressed: A";
                 button_pressed_text.setText("Pressed: A");
             }
         });
@@ -45,6 +54,7 @@ public class ClickyClick extends AppCompatActivity {
         button_b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                last_button_pressed = "Pressed: B";
                 button_pressed_text.setText("Pressed: B");
             }
         });
@@ -52,6 +62,7 @@ public class ClickyClick extends AppCompatActivity {
         button_c.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                last_button_pressed = "Pressed: C";
                 button_pressed_text.setText("Pressed: C");
             }
         });
@@ -60,6 +71,7 @@ public class ClickyClick extends AppCompatActivity {
         button_d.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                last_button_pressed = "Pressed: D";
                 button_pressed_text.setText("Pressed: D");
             }
         });
@@ -67,6 +79,7 @@ public class ClickyClick extends AppCompatActivity {
         button_e.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                last_button_pressed = "Pressed: E";
                 button_pressed_text.setText("Pressed: E");
             }
         });
@@ -74,6 +87,7 @@ public class ClickyClick extends AppCompatActivity {
         button_f.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                last_button_pressed = "Pressed: F";
                 button_pressed_text.setText("Pressed: F");
             }
         });
@@ -85,5 +99,12 @@ public class ClickyClick extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString("last_button_pressed", last_button_pressed);
     }
 }
